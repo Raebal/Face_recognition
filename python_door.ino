8e46f77a715795a369ac27b9dc3199026c72901c
Raebal <55364054+Raebal@users.noreply.github.com>
2019-10-07 17:46:11 +0300

    Add files via upload

---
+++ python_door.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+String pos;
+int e = 0;
+Servo myservo;
+void setup() {
+myservo.attach(3);
+Serial.begin(9600); }
+void loop() {
+if (Serial.available()>=1) {
+pos = Serial.readString();
+e = pos.toInt();
+myservo.write(e);
+delay(2000); } }
+
